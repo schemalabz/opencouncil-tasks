ab0bf50be2e8f0da338beaa9b872144afc17986a
Christos Porios <christos.porios@gmail.com>
2024-09-06 13:46:06 +0300

    increase express request limit

--- src/utils.ts
+++ src/utils.ts
@@ -22,7 +22,7 @@ export const getExpressAppWithCallbacks = () => {
     const app = express();
     const port = process.env.PORT || 3000;
 
-    app.use(express.json());
+    app.use(express.json({ limit: '100mb' }));
 
     const callbackRouter = Router();
     const callbackServer = CallbackServer.getInstance(callbackRouter, '/callback');
